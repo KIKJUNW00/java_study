96c32f6d312543bcd69d21b9f0f48bb12d54b42e
user <user@DESKTOP-G5IVKN9>
2024-08-26 17:16:21 +0900

    javaStudy_0826

---
+++ src/chap2_dataType/Test2.java
@@ -0,0 +1,15 @@
+package chap2_dataType;
+
+public class Test2 {
+	public static void main(String[] args) {
+		int deposit = 5000;
+		System.out.println(deposit);// 정수 변수
+		/*
+		 * 변수선언
+		 */
+		System.out.println("\n출력결과");// 정수 변수
+		
+		
+	}
+
+}
