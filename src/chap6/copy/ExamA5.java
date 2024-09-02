eb178691b6db8a913da85b76361ce795894d272c
KIKJUNW00 <kimjun9758@naver.com>
2024-09-02 14:16:55 +0900

    JavaStudy_0902

--- src/chap6/copy/ExamA5.java
+++ src/chap6/copy/ExamA5.java
@@ -1,27 +1,15 @@
-package chap5_Class;
+package chap6.copy;
 
 import java.util.Iterator;
 
 //	클래스 Student를 사용하여 학생 이름, 과목배열, 성적배열을 객체로 생성
 //	필드
-class Student12 {
+class Student11 {
 	private String name;
 	private String subject[];
 	private int scores[];
 	private int criteria[];
-	
 
-
-public Student12(String name, String[] subject, int[] scores, int[] criteria) {
-		super();
-		this.name = name;
-		this.subject = subject;
-		this.scores = scores;
-		this.criteria = criteria;
-	}
-
-//	name, criteria, scores를 모두 private으로 선언
-//	 필요한 getter 또는 setter 만 추가
 	public String getName() {
 		return name;
 	}
@@ -53,26 +41,31 @@ public Student12(String name, String[] subject, int[] scores, int[] criteria) {
 	public void setCriteria(int[] criteria) {
 		this.criteria = criteria;
 	}
-	
 
-//	public int sumScore(Student12[] students) {
-//		int sum = 0;
-//		for (int i = 0; i < students[0].scores.length; i++) {
-////			sum += students[i].scores[];
-//		}
-//	}
-	
+	public int sumScore() {
+		int sum = 0;
+		for (int i = 0; i < scores.length; i++) {
+			sum += scores[i];
+		}
+		return sum;
+	}
 
 	public int average() {
 		int avg = 0;
-//		avg = sumScore() / scores.length;
+		avg = sumScore() / scores.length;
 		return avg;
 	}
 
 	void isPassed() {
-		
+		for (int i = 0; i < subject.length; i++) {
+//			String
+		}
+//		for (int j = 0; j < subjs.length; j++) {
+//			String hapbul = (scores[i][j] >= criteria[j]) ? "통과" : "과락";
+//			System.out.println("\t" + subjs[j] + " : " + scores[i][j] + " => " + hapbul);
+//		}
 	}
-
+//}
 ////	name, criteria, scores를 모두 private으로 선언
 ////	 필요한 getter 또는 setter 만 추가
 ////	메서드
@@ -80,7 +73,7 @@ public Student12(String name, String[] subject, int[] scores, int[] criteria) {
 //
 }
 
-public class ExamA4 {
+public class ExamA5 {
 	public static void main(String[] args) {
 
 		// 과목 배열 정의
@@ -88,16 +81,16 @@ public class ExamA4 {
 		// 과목별 통과 기준 배열 정의
 		int[] criteria = { 50, 60, 70, 80, 90 };
 		// 학생 배열 생성
-		Student12[] students = {
-				new Student12("홍길동", subjects, criteria, new int[] { 85, 90, 78, 88, 92 }),
-				new Student12("김유신", subjects, criteria, new int[] { 75, 80, 85, 90, 95 }),
-				new Student12("계백", subjects, criteria, new int[] { 65, 70, 75, 80, 85 }),
-				new Student12("강감찬", subjects, criteria, new int[] { 95, 92, 88, 84, 91 }),
-				new Student12("을지문덕", subjects, criteria, new int[] { 88, 76, 85, 79, 90 }) 
+		Student11[] students = {
+//				new Student("홍길동", subjects, criteria, new int[] { 85, 90, 78, 88, 92 }),
+//				new Student("김유신", subjects, criteria, new int[] { 75, 80, 85, 90, 95 }),
+//				new Student("계백", subjects, criteria, new int[] { 65, 70, 75, 80, 85 }),
+//				new Student("강감찬", subjects, criteria, new int[] { 95, 92, 88, 84, 91 }),
+//				new Student("을지문덕", subjects, criteria, new int[] { 88, 76, 85, 79, 90 }) 
 		};
 
 //		각 학생의 성적 테이블 출력: 학생 이름, 총점, 평균, 과목별 점수, 과목별 통과여부
 //		과목별 최대/최소 점수 및 해당 학생 이름 출력: 과목명, 최대 점수, 최소 점수, 최대점수 학생, 최소점수 학생
-	
+
 	}
 }
