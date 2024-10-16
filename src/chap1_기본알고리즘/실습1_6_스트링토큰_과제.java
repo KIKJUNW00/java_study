05ef2df66dee3982b4696ca49f689e1619032ac1
KIKJUNW00 <kimjun9758@naver.com>
2024-10-16 18:06:08 +0900

    알고리즘1_2실습과제

--- src/chap1_기본알고리즘/실습1_6_스트링토큰_과제.java
+++ src/chap1_기본알고리즘/실습1_6_스트링토큰_과제.java
@@ -4,55 +4,61 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 실습1_6_스트링토큰_과제 {
-/*
- * StringTokenizer(input), countTokens(), hasMoreTokens(), nextToken()
- * parseDouble(stringArray[i])
- */
-    // 문자열을 실수로 추출하여 배열에 저장하고 정렬하는 함수
+	/*
+	 * StringTokenizer(input), countTokens(), hasMoreTokens(), nextToken()
+	 * parseDouble(stringArray[i])
+	 */
+	// 문자열을 실수로 추출하여 배열에 저장하고 정렬하는 함수
 	private static String[] extractSortStrings(String msg) {
 		StringTokenizer token1 = new StringTokenizer(msg);
 		String[] arr = new String[token1.countTokens()];
-		
+
 		int index = 0;
 		while (token1.hasMoreTokens()) {
 			arr[index++] = token1.nextToken();
-			
+
 		}
-		
+
 		Arrays.sort(arr);
-		
+
 		return arr;
 	}
 
-    // 문자열 배열을 실수 배열로 변환한 후 정렬하는 함수
-	private static double[] convertSortToDouble(String[] sortedStringArray) {
-//		double[] doubleArray
-		return null;
+	// 문자열 배열을 실수 배열로 변환한 후 정렬하는 함수
+	private static double[] convertSortToDouble(String[] ssr) {
+		double[] doubleArray = new double[ssr.length];
+		for (int i = 0; i < ssr.length; i++) {
+
+			doubleArray[i] = Double.parseDouble(ssr[i]);
+		}
+		// 문자열 배열을 실수 배열로 변환
+		Arrays.sort(doubleArray);
+		return doubleArray;
+
 	}
-    // 문자열 배열을 실수 배열로 변환
-        
 
-    // 문자열 배열 출력 함수
-    
+	// 문자열 배열 출력 함수
+	private static void printStringArray(String[] sortedStr) {
+		System.out.println(Arrays.toString(sortedStr));
+	}
 
-    // 실수 배열 출력 함수
-    
+	// 실수 배열 출력 함수
+	private static void printDoubleArray(double[] sortedDoubleArray) {
+		System.out.println(Arrays.toString(sortedDoubleArray));
+	}
 
-    public static void main(String[] args) {
-        String msg = "3.24 3.34156 1141.56 214. 0.0314156 54.12f";
-        
-        // 실수를 문자열로 추출하고 정렬 후 출력
-        String[] sortedStringArray = extractSortStrings(msg);
-        System.out.println("정렬 스트링 배열:");
-//        printStringArray(sortedStringArray);
-        
-        // 문자열 배열을 실수 배열로 변환하고 정렬 후 출력
-        double[] sortedDoubleArray = convertSortToDouble(sortedStringArray);
-        System.out.println("정렬 실수 배열:");
-//        printDoubleArray(sortedDoubleArray);
-    }
+	public static void main(String[] args) {
+		String msg = "3.24 3.34156 1141.56 214. 0.0314156 54.12f";
 
-	
+		// 실수를 문자열로 추출하고 정렬 후 출력
+		String[] sortedStringArray = extractSortStrings(msg);
+		System.out.println("정렬 스트링 배열:");
+		printStringArray(sortedStringArray);
 
-	
+		// 문자열 배열을 실수 배열로 변환하고 정렬 후 출력
+		double[] sortedDoubleArray = convertSortToDouble(sortedStringArray);
+		System.out.println("정렬 실수 배열:");
+		printDoubleArray(sortedDoubleArray);
+	}
+
 }
