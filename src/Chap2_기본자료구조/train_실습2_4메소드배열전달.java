95c60596ffd2313fe4ea02b868fa89891b896eee
KIKJUNW00 <kimjun9758@naver.com>
2024-10-17 14:12:38 +0900

    2_3_4실습_자료구조알고리즘

--- src/Chap2_기본자료구조/train_실습2_4메소드배열전달.java
+++ src/Chap2_기본자료구조/train_실습2_4메소드배열전달.java
@@ -5,6 +5,7 @@ package Chap2_기본자료구조;
  * function parameters를 작성할 수 있어야 한다 
  */
 
+import java.util.Arrays;
 import java.util.Random;
 public class train_실습2_4메소드배열전달 {
 	static int top = 0;
@@ -22,15 +23,22 @@ public class train_실습2_4메소드배열전달 {
 		
 	}
 	
-
-	private static void reverse(int[] data) {
-		
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1]=a[idx2];
+		a[idx2]=t;
+	}
+	
+	static void reverse(int[] data) {
+		for (int i = 0; i < data.length/2; i++) {
+			swap(data, i ,data.length-i-1);
+		}
 		
 	}
 
 	public static void showData(String str, int[] a) {
-		//top 갯수까지 출력한다 [1,2,3]등으로 출력하도록 작성
-		
+		//top 갯수까지 출력한다 [1,2,3]등으로 출력하도록 작성	
+		System.out.print(str+": "+Arrays.toString(a)+", "+a.length+"개");
 		
 	}
 	public static void inputData(int[] a) {//교재 63 - 난수의 생성
@@ -44,11 +52,26 @@ public class train_실습2_4메소드배열전달 {
 	}
 	public static int findMax(int[] a) {
 		//최대값을 리턴한다 
-		return top;
+		int max = a[0];
+		
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+		return max;
 	}
 	public static boolean findValue(int[] a, int i) {
 		//items[]에 value 값이 있는지를 찾아 존재하면 true, 없으면 false로 리턴
+		for (int j = 0; j < a.length; j++) {
+			if (a[j] == i) {
+				
+				return true;
+			} 			
+		}
 		return false;
+		
+		
 	}
 
 }
